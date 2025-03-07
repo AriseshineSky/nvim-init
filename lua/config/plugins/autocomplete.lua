970ab7553a37b0feff5b6502e803a762067ecb69
Sky <befruitful12@gmail.com>
2025-03-06 23:14:37 -0600

    add cmdline

--- lua/config/plugins/autocomplete.lua
+++ lua/config/plugins/autocomplete.lua
@@ -50,6 +50,7 @@ M.config = {
 	dependencies = {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-nvim-lua",
 		"hrsh7th/cmp-calc",
