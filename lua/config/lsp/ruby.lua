152c548e37183f24e326ccfea1e50187154f7c46
SmileintheSky <befruitful12@gmail.com>
2025-04-09 12:57:28 -0500

    change ruby lsp

--- lua/config/lsp/ruby.lua
+++ lua/config/lsp/ruby.lua
@@ -2,10 +2,14 @@ return {
 	setup = function(lspconfig, lsp)
 		lspconfig.ruby_lsp.setup({
 			init_options = {
+				["Ruby LSP Rails"] = {
+					enablePendingMigrationsPrompt = false,
+				},
 				formatter = 'standard',
-				linters = { 'standard' }
+				linters = { 'standard' },
 			},
 			filetypes = { 'ruby' },
+			root_dir = lspconfig.util.root_pattern("Gemfile", ".git", ".ruby-version", "config/application.rb"),
 		})
 	end
 }
